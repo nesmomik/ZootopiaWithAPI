523bf56c5109c306177d7be36233b24378eee27f
Michael Thomsen <nesmomik@gmail.com>
2025-11-15 22:02:06 +0100

    Added additional fields

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -30,7 +30,7 @@ def load_data(file_path):
 
 
 def format_data():
-    """Aggregate and returns a string from the data"""
+    """Aggregates and returns a string from the data"""
     animals_data = load_data(JSON_DATA_FILE)
 
     data_string = ''
@@ -46,11 +46,21 @@ def serialize_animal(animal):
     data_string += '<li class="cards__item">'
     data_string += f'<div class="card__title">{animal.get('name')}</div>\n'
     data_string += '<p class="card__text">'
+    data_string += f"<strong>Scientific Name:</strong> {animal.get('taxonomy').get('scientific_name')}<br/>\n"
     if animal.get("characteristics").get("diet") is not None:
         data_string += f"<strong>Diet:</strong> {animal.get('characteristics').get('diet')}<br/>\n"
-    data_string += f"<strong>Location:</strong> {animal.get('locations')[0]}<br/>\n"
+    data_string += f"<strong>Location(s):</strong> {animal.get('locations')[0]}"
+    for i in range(1, len(animal.get('locations'))):
+        data_string += f", {animal.get('locations')[i]}"
+    data_string += "<br/>\n"
+    if animal.get("characteristics").get("weight") is not None:
+        data_string += f"<strong>Weight:</strong> {animal.get('characteristics').get('weight')}<br/>\n"
     if animal.get("characteristics").get("type") is not None:
         data_string += f"<strong>Type:</strong> {animal.get('characteristics').get('type')}<br/>\n"
+    if animal.get("characteristics").get("temperament") is not None:
+        data_string += f"<strong>Temperament:</strong> {animal.get('characteristics').get('temperament')}<br/>\n"
+    if animal.get("characteristics").get("slogan") is not None:
+        data_string += f"<strong>Slogan:</strong> {animal.get('characteristics').get('slogan')}<br/>\n"
     data_string += '</li>'
     
     return data_string
