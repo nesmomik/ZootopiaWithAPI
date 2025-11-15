96f631adfbe8a8e7fbab75b1af3c6731573a25cc
Michael Thomsen <nesmomik@gmail.com>
2025-11-15 20:01:04 +0100

    Added function to read template, moved data formating to separate function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,22 +1,40 @@
 import json
 
+
 def load_data(file_path):
-    """ Loads a json file """
+    """Returns the date from the json file as dict"""
     with open(file_path, "r") as handle:
         return json.load(handle)
 
- 
-def main():
-    animals_data = load_data('animals_data.json')
+
+def read_template(file_path):
+    """Returns the content of the template file as string"""
+    with open(file_path, "r") as handle:
+        return handle.read()
+        
+
+def format_data():
+    """ Creates and returns a string from the data"""
+    animals_data = load_data("animals_data.json")
+
+    data_string = ""
 
     for animal in animals_data:
-        print(animal)
-        print(f"Name: {animal.get('name')}")
-        if animal.get('characteristics').get('diet') is not None:
-            print(f"Diet: {animal.get('characteristics').get('diet')}")
-        print(f"Location: {animal.get('locations')[0]}")
-        if animal.get('characteristics').get('type') is not None:
-            print(f"Type: {animal.get('characteristics').get('type')}")
+        data_string += f"Name: {animal.get('name')}\n"
+        if animal.get("characteristics").get("diet") is not None:
+            data_string += f"Diet: {animal.get('characteristics').get('diet')}\n"
+        data_string += f"Location: {animal.get('locations')[0]}\n"
+        if animal.get("characteristics").get("type") is not None:
+            data_string += f"Type: {animal.get('characteristics').get('type')}\n"
+    
+    return data_string
+    
+
+def main():
+
+    print(format_data())
+    print(read_template("animals_template.html"))
+
 
 if __name__ == "__main__":
     main()
